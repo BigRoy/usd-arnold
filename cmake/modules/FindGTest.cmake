fbb57ccf244578363e2932576c1e3259043e370e
Roy Nieterau <roy_nieterau@hotmail.com>
2019-11-07 13:00:36 +0100

    Fix FindGTest for Windows build

--- cmake/modules/FindGTest.cmake
+++ cmake/modules/FindGTest.cmake
@@ -1,12 +1,21 @@
 # Simple module to find Google Test.
 
 find_path(GTEST_INCLUDE_DIR gtest/gtest.h
+          HINTS "${GTEST_ROOT}/include"
           DOC "Google Test include directory")
 
-find_library(GMOCK_LIBRARY libgmock.a)
-find_library(GMOCK_MAIN_LIBRARY libgmock_main.a)
-find_library(GTEST_LIBRARY libgtest.a)
-find_library(GTEST_MAIN_LIBRARY libgtest_main.a)
+find_library(GMOCK_LIBRARY 
+             NAMES libgmock.a gmock.lib
+             HINTS "${GTEST_ROOT}/lib")
+find_library(GMOCK_MAIN_LIBRARY 
+             NAMES libgmock_main.a gmock_main.lib
+             HINTS "${GTEST_ROOT}/lib")
+find_library(GTEST_LIBRARY 
+             NAMES libgtest.a gtest.lib
+             HINTS "${GTEST_ROOT}/lib")
+find_library(GTEST_MAIN_LIBRARY 
+             NAMES libgtest_main.a gtest_main.lib
+             HINTS "${GTEST_ROOT}/lib")
 
 set(GTEST_LIBRARIES "${GMOCK_LIBRARY};${GMOCK_MAIN_LIBRARY};${GTEST_LIBRARY};${GTEST_MAIN_LIBRARY}")
 
